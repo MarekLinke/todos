03a5004bce193630b0ed47908f2a32616011a333
Dmitrii <ryen@mail.ru>
2026-06-01 18:23:04 +0300

    редактирование App.js разобрать код в нём ошибка

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import todos from './todos.js';
+import TodoList from './TodoList.js';
 
 export default function App() {
   return (
@@ -11,7 +12,7 @@ export default function App() {
         </div>
       </nav>
       <main className="content px-6 py-6">
-        <h1>Todos</h1>
+        <TodoList list={todos} />
       </main>
     </div>
   );
